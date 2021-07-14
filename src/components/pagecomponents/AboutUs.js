036f8fb212a478b38f24fa92e20e6262e5073ae8
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-07-13 21:37:29 -0500

    add: aboutus and CNAME

--- src/components/pagecomponents/AboutUs.js
+++ src/components/pagecomponents/AboutUs.js
@@ -5,7 +5,7 @@ function AboutUs(){
         <section className="aboutusHeader">
             <h1 className="heading">ABOUT US</h1>
             <div className="centering">
-                <p className="whatweare"> We are an open source team that are creating aircraft and scenery for Microsoft Flight Simulator. We are currently creating UAAA (Almaty International Airport) located in Almaty, Kazakhstan. On top of that, we are also creating a free Boeing 777-300ER, known as the PPS777 Project.</p>
+                <p className="whatweare"> In Feburary 2021, we announced the start of the group. At the time, we also announced our first project, which is the PPS777-300ER or PPS777 for short. The project was the kickstart of our development for Microsoft Flight Simulator and introduced experienced developers to a different environment of development. Additionally, we have our Pinpoint Asphalt scenery projects. And to put the cherry on top, this is all going to be free.</p>
             </div>
             
         </section>
