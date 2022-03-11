ef8fb30eeefeaf96169217e67b07089e0dca08c3
danisbored <danopperm@gmail.com>
2022-03-11 19:11:43 +0100

    Removed pages

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import PageNotFound from './pages/PageNotFound'
 import Home from './pages/Home'
-import Updates from './pages/Updates'
-import Downloads from './pages/Downloads';
 import Tos from './pages/Tos';
 import Privacy from './pages/Privacy';
 
@@ -16,8 +14,6 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/updates" component={Updates }/>
-        <Route exact path="/downloads" component={Downloads }/>
         <Route exact path="/privacy" component={Privacy }/>
         <Route exact path="/tos" component={Tos }/>
         <Route component={PageNotFound}/>
